3eab891ed99a6d0ef1873f7a6f2ed21603e5f281
Andreii1414 <95772732+Andreii1414@users.noreply.github.com>
2023-02-26 12:56:47 +0200

    Add files via upload

---
+++ Lab 1/src/Bonus/Ex1Bonus.java
@@ -0,0 +1,69 @@
+package Bonus;
+
+public class Ex1Bonus {
+    public static void main(String[] args) {
+
+        if(args.length != 1) //verific ca exista un singur argument (n)
+        {
+            System.out.println("Niciun argument");
+            return;
+        }
+
+        int n;
+
+        try{ //validez argumentul (daca e intreg)
+            n = Integer.parseInt(args[0]);
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            return; //nu e intreg
+        }
+
+        int[][] adjacencyMatrix = new int[n][n];
+
+        adjacencyMatrix[0][n-1] = 1; //legatura intre nodul 0 si n-1 (nodul anterior al nodului 0)
+        adjacencyMatrix[n-1][0] = 1; //legatura intre nodul n-1 si 0 (nodul urmator al nodului n-1) + graf neorientat
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            adjacencyMatrix[i][i+1] = 1; //fac legaturi intre nodul curent si nodul anterior si nodul curent si nodul urmator
+            adjacencyMatrix[i+1][i] = 1; //- graf neorientat
+        }
+
+        int[][] An = adjacencyMatrix;
+
+        System.out.println("A = ");
+        Print(adjacencyMatrix, n);
+
+        for(int i = 2; i <= n; i++) // calculez A2...An
+        {
+            An = mul(adjacencyMatrix,An, n);
+            System.out.println("A ^ " + i + "=");
+            Print(An, n);
+        }
+    }
+
+    public static void Print(int[][] A, int n)
+    {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int[][] mul(int[][] A, int[][] B, int n)
+    {
+        int[][] C = new int[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                for(int k = 0; k < n; k++)
+                {
+                    C[i][j] = C[i][j] + A[i][k] * B[k][j];
+                }
+        return C;
+    }
+}
---
+++ Lab 1/src/Homework/Homework.java
@@ -0,0 +1,67 @@
+package Homework;
+public class Homework {
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+
+        if(args.length != 1) //verific ca exista un singur argument (n)
+        {
+            System.out.println("Niciun argument");
+            return;
+        }
+
+        int n;
+
+        try{ //validez argumentul (daca e intreg)
+            n = Integer.parseInt(args[0]);
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            return; //nu e intreg
+        }
+
+        int[][] latinSquare = new int[n][n];
+
+        //explicatie pt n = 5
+        int k1 = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int k2 = 0;
+            for(int j = n - 1; j >= 0; j--){
+                if(j + 1 - k1 <= 0){ //2. in loc de 0 de mai jos pun 5, in loc de -1 pun 4, etc
+                    latinSquare[i][j] = n - k2;
+                    k2++;
+                }
+                else latinSquare[i][j] = j + 1 - k1;  //1. Linia 0: 1 2 3 4 5; Linia 1: *0* 1 2 3 4 ; Linia 2: *-1* *0* 1 2 3;...
+            }
+            k1++;
+        }
+
+      if(n < 100)
+      {
+          for(int i = 0; i < n; i++) //printez matricea
+          {
+              for(int j = 0; j < n; j++)
+                  System.out.print(latinSquare[i][j]);
+              System.out.println();
+          }
+      }
+
+        for(int i = 0; i < n; i++) {
+            String line = new String("");
+            String col = new String("");
+            for (int j = 0; j < n; j++) { //concatenez linia i, respectiv coloana i
+                line = line + latinSquare[i][j];
+                col = col + latinSquare[j][i];
+            }
+            if (n < 100) { //pentru n >= 100 nu mai printez liniile si coloanele concatenate
+                System.out.println("Line " + i + " :" + line);
+                System.out.println("Column: " + i + " :" + col);
+            }
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)+ " milliseconds");
+    }
+}
