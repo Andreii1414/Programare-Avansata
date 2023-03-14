7952b2388a12f6bbf4963e11640486ab0c9d41bc
Andreii1414 <95772732+Andreii1414@users.noreply.github.com>
2023-03-14 07:09:25 +0200

    Add files via upload

---
+++ Lab4/src/main/java/org/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.Compulsory","l":"Main"},{"p":"org.example.Compulsory","l":"Project"},{"p":"org.example.Compulsory","l":"Student"}];updateSearchResults();
