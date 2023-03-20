bb0a9e4fed58ade8df5c465ce844c9329a6b792e
Andreii1414 <95772732+Andreii1414@users.noreply.github.com>
2023-03-20 08:49:13 +0200

    Add files via upload

---
+++ Lab4/target/classes/org/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.Homework","l":"Main"},{"p":"org.example.Homework","l":"Problem"},{"p":"org.example.Homework","l":"Project"},{"p":"org.example.Homework","l":"Student"}];updateSearchResults();
